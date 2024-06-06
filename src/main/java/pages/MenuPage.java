f258a0e33dc345a28b5e234c9c2e811d7a9b4c28
unknown <irinaandrei83@gmail.com>
2024-06-06 09:17:19 +0300

    commit inititial

---
+++ src/main/java/pages/MenuPage.java
@@ -0,0 +1,17 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+
+import selenium.utils.SeleniumWrappers;
+
+public class MenuPage extends SeleniumWrappers{
+	
+	//public WebDriver driver;
+	
+	public MenuPage(WebDriver driver) {
+		//this.driver = driver;
+		super(driver);	//constructor
+	}
+	
+
+}
---
+++ src/main/java/selenium/utils/BaseTest.java
@@ -0,0 +1,78 @@
+package selenium.utils;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.time.Duration;
+import java.util.Date;
+
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.ITestResult;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Parameters;
+
+import com.google.common.io.Files;
+
+
+
+public class BaseTest {
+
+	//initilizam si inchidem driverul dupa fiecare executie
+	//folosim before and after class
+	
+	public WebDriver driver; //am importat WebDriver
+
+	
+	@Parameters({"url"})
+	@BeforeClass
+	public void setup(String appUrl) { //deschide browserul si navigheaza catre pagina noastra
+		
+		driver = new ChromeDriver();
+		driver.manage().window().maximize(); //mereu pornim pagina web pe pagina maximizata
+											 //pentru a gasi toate butoanele cu locatorii (e diferit pe telefon vs monitor)
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		driver.get(appUrl);
+		
+		
+	}
+	//@AfterClass
+	public void tearDown() throws InterruptedException {
+		Thread.sleep(5000); //lasa browserul deschis 5000 secunde
+		driver.quit(); //inchide toate taburile
+		//driver.close();//inchide tabul curent
+	}
+	
+	@AfterMethod
+	public void recordFailure(ITestResult result) {   //listener
+		//metoda se va executa dupa rularea fiecarei metode @Test
+		//daca metoda este FAILED -> doar atunci face poza
+		
+		 if(ITestResult.FAILURE == result.getStatus()) {
+			 TakesScreenshot sc = (TakesScreenshot) driver;
+			 File poza = sc.getScreenshotAs(OutputType.FILE);
+			 String timeStamp = new SimpleDateFormat("yyyy.MM.dd.hh.mm.ss").format(new Date());
+			 
+			 try {
+				 Files.copy(poza, new File("poze/"+result.getName()+"-"+timeStamp+".png")); 
+				 //pt ca fiecare poza salvata sa aiba un nume unic de fiecare data, altfel se suprascrie
+				 System.out.println("Picture saved");
+				 
+			 }catch(IOException e) {
+				 System.out.println("Picture could not be saved");
+				 e.printStackTrace();
+			 }
+			 
+		 }
+		
+		
+	}
+	
+}
